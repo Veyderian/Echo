237d6a482c61c36f7177f5fd5f4348f9057f08eb
Olga Korol <oggi.fine@gmail.com>
2023-08-24 12:37:41 +0300

    tests added

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 
@@ -37,7 +37,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет"))
+                .body("data", equalTo("Здравствуйте"))
         ;
     }
 }
